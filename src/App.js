7f01fcc58f551a08ad89bf6169206645ec6f555c
Yash Kapoor <yashkapoor.dram@gmail.com>
2021-06-30 07:41:16 +0530

    hamburger-menu 1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,37 @@ import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
 import { NavBar } from "./MyComponent/NavBar";
 import { Header } from "./MyComponent/Header";
 import { Home } from "./MyComponent/Home";
+import { Menu } from "./MyComponent/Menu";
+import { useState } from "react";
 
 //importing the images
 import homeImg from "./Images/1.png";
 
 function App() {
+  const [menu, setMenu] = useState(true);
+  const ToggleMenu = () => {
+    setMenu(!menu);
+    console.log(menu);
+  };
   return (
     <div className="App">
       <Router>
-        <header className="header">
-          <NavBar />
-          <Header imgs={homeImg} />
-        </header>
-        <div className="content">
-          <Home />
-        </div>
+        <Switch>
+        {!menu ? <Menu /> : ""}
+          <Route exactpath="/">
+            <header className="header">
+              <NavBar
+                menu={() => {
+                  ToggleMenu();
+                }}
+              />
+              <Header imgs={homeImg} />
+            </header>
+            <div className="content">
+              <Home />
+            </div>
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
--- src/MyComponent/NavBar.js
+++ src/MyComponent/NavBar.js
@@ -2,15 +2,16 @@ import React from 'react'
 import {BrowserRouter as Router,Link} from "react-router-dom"
 import "../css/navbar.css"
 
-export const NavBar = () => {
+export const NavBar = (props) => {
     return (
         <div className="content-navigation">
-            <div className="brand-name">Brand name</div>
+            <div className="brand-name">Title</div>
             <div className="links">
                 <Link to="/">Home</Link>
                 <Link to="/">About</Link>
                 <Link to="/">Blogs</Link>
                 <Link to="/">Gallery</Link>
+                <button onClick={props.menu} >Button</button>
             </div>
         </div>
     )
